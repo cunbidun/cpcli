e9b665981aa084a9da1c7c102f79d606d5671c04
Duy Pham <cunbidun@gmail.com>
2021-09-12 10:24:54 -0400

    Set stack size to unlimited

--- TODO.md
+++ TODO.md
@@ -1,1 +1,2 @@
 1. [] Update README
+2. [] check for update by timestamps  
--- main.sh
+++ main.sh
@@ -10,5 +10,6 @@ if [ $num_args == 0 ]; then
 	./cpcli_app ./project_config.json
 else
 	ROOT=$(realpath "$1")
+	ulimit -s unlimited
 	./cpcli_app "$ROOT" ./project_config.json "$2"
 fi
