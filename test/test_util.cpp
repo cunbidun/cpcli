4b3e9ddbc19579ff9a63c7a94eaa6f6d374c7b26
Duy Pham <cunbidun@gmail.com>
2022-10-18 23:05:13 -0500

    Add tests

--- test/test_util.cpp
+++ test/test_util.cpp
@@ -8,10 +8,14 @@
 namespace fs = std::filesystem;
 using std::string;
 
+TEST(test_utils, test_gen_string_length_20) {
+  string s = gen_string_length_20();
+  EXPECT_EQ(s.size(), 20);
+}
+
 TEST(TestUtil, TestEmptyFile) {
   auto cache_dir = fs::temp_directory_path() / "cpcli_test";
   string file_name = gen_string_length_20();
-  EXPECT_EQ(file_name.size(), 20) << "gen_string_length_20 doesn't return string with length of 20";
   auto file_path = cache_dir / file_name;
   create_empty_file(file_path);
   EXPECT_EQ(is_empty_file(file_path), true);
@@ -22,7 +26,34 @@ TEST(TestUtil, TestJoin) {
   EXPECT_EQ(join(x), "a b c") << "join on ['a' 'b' 'c'] not working.";
 }
 
+TEST(TestUtil, TestJoinEmpty) {
+  std::vector<string> x;
+  EXPECT_EQ(join(x), "") << "join on empty vector not working.";
+}
+
+TEST(TestUtil, TestJoinOneElement) {
+  std::vector<string> x = {"a"};
+  EXPECT_EQ(join(x), "a") << "join on one element vector not working.";
+}
+
 TEST(TestUtil, TestSystemWarper) {
   int output = system_warper("ls");
   EXPECT_EQ(output, 0);
+}
+
+TEST(TestUtil, TestSystemWarperInvalidCommand) {
+  int output = system_warper("invalid_command");
+  EXPECT_NE(output, 0);
+}
+
+TEST(TestUtil, TestTrim) {
+  string s = "  hello  ";
+  trim(s);
+  EXPECT_EQ(s, "hello");
+}
+
+TEST(TestUtil, TestTrimEmpty) {
+  string s = "";
+  trim(s);
+  EXPECT_EQ(s, "");
 }
